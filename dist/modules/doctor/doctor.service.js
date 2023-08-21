cfda036b83186f68873d92e09be672d65916fd76
JOY-JB <joybarua0058@gmail.com>
2023-08-21 20:09:38 +0600

    new build

--- dist/modules/doctor/doctor.service.js
+++ dist/modules/doctor/doctor.service.js
@@ -8,6 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.doctorService = void 0;
 const client_1 = require("@prisma/client");
@@ -20,7 +31,11 @@ const addDoctor = (data) => __awaiter(void 0, void 0, void 0, function* () {
 });
 const getAllDoctors = () => __awaiter(void 0, void 0, void 0, function* () {
     const doctors = yield prisma.doctor.findMany();
-    return doctors;
+    const doctorsWithoutPassword = doctors.map((doctor) => {
+        const { password } = doctor, doctorWithoutPassword = __rest(doctor, ["password"]);
+        return doctorWithoutPassword;
+    });
+    return doctorsWithoutPassword;
 });
 exports.doctorService = {
     addDoctor,
